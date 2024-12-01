65f4ecda69227e43e3f719bc632604c392b2bf9a
wylupek <filip@tapoczta.pl>
2024-12-01 12:40:32 +0100

    Add ssl/

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI, Request
 import uvicorn
-import os
-from dotenv import load_dotenv
-
 from utils import database_utils
 from utils.inference import inference as inference_fun
 from utils.train import train as train_fun, LoadMode
@@ -54,9 +51,8 @@ async def inference(request: Request, username: str):
 
 
 def main():
-    load_dotenv()
-    ssl_key_file = os.getenv("SSL_KEY_FILE_PATH")
-    ssl_cert_file = os.getenv("SSL_CERT_FILE_PATH")
+    ssl_key_file = "ssl/key.pem"
+    ssl_cert_file = "ssl/cert.pem"
     print(ssl_key_file, ssl_cert_file)
     uvicorn.run(app, host="192.168.1.100", port=8000, ssl_keyfile=ssl_key_file, ssl_certfile=ssl_cert_file)
 
---
+++ ssl/README.md
@@ -0,0 +1,11 @@
+### Generate SSL
+Generate all necessary certificates using the following commands:
+```
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -config san.cnf -extensions req_ext
+openssl x509 -outform der -in cert.pem -out cert.crt
+```
+### Server
+Put `key.pem` and `cert.pem` into `ssl/`.
+
+### Client
+Create a `res/raw` directory in your Android project, and place `cert.crt` there.
